6eb4029356de44635f842908195ed46c3204b662
Chandu Surisetty <68140710+chandusurisetty@users.noreply.github.com>
2023-08-23 15:54:35 +0530

    Update RepeatedSubstringPattern.py

--- RepeatedSubstringPattern.py
+++ RepeatedSubstringPattern.py
@@ -1,17 +1,2 @@
-class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
-        getSub=""
-        for i in s:
-            if getSub:
-                if i ==getSub[0]:
-                    
-                    if not s.replace(getSub,""):
-                        return True
-                    getSub+=i
-                else:
-                    getSub+=i
 
-            else:
-                getSub=i
-        return False
                 
